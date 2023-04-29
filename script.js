db19380682588a5f10eb50edd91919665e80ed1b
daniell wendyson <daniellwendyson@gmail.com>
2023-04-29 17:44:06 -0300

    fix

--- script.js
+++ script.js
@@ -7,34 +7,34 @@ let time = 60;
 let timer = "";
 let mistakes = 0;
 
-//Display random quotes
+
 const renderNewQuote = async () => {
-    //Fetch content from quote api url
+    
     const response = await fetch(quoteApiUrl);
     let data = await response.json();
     quote = data.content;
 
-    //Array of chars in quote
+   
     let arr = quote.split("").map((value) => {
         return "<span class='quote-chars'>" + value + "</span>";
     });
     quoteSection.innerHTML += arr.join("");
 };
 
-//Logic to compare input words with quote
+
 userInput.addEventListener("input", () => {
     let quoteChars = document.querySelectorAll(".quote-chars");
     quoteChars = Array.from(quoteChars);
 
-    //Array of user input chars
+    
     let userInputChars = userInput.value.split("");
-    //Loop through each char in quote
+    
     quoteChars.forEach((char, index) => {
-        //Check chars with quote chars
+        
         if (char.innerText == userInputChars[index]) {
             char.classList.add("success");
         }
-        //If user hasn't entered anything or backspaced
+        
         else if (userInputChars[index] == null) {
             if (char.classList.contains("success")) {
                 char.classList.remove("success");
@@ -42,22 +42,22 @@ userInput.addEventListener("input", () => {
                 char.classList.remove("fail");
             }
         }
-        //if user entered wrong char
+        
         else {
             if (!char.classList.contains("fail")) {
-                //increament and displaying mistakes
+                
                 mistakes++;
                 char.classList.add("fail");
             }
             document.getElementById("mistakes").innerText = mistakes;
         }
 
-        //Return true if all chars are correct
+        
         let check = quoteChars.every((element) => {
             return element.classList.contains("success");
         });
 
-        //End test if all chars are correct
+        
         if (check) {
             displayResult();
         }
@@ -66,25 +66,24 @@ userInput.addEventListener("input", () => {
 
 });
 
-//Update timer
+
 function updateTimer() {
     if (time == 0) {
-        //End test if reaches 0
+        
         displayResult();
     } else {
         document.getElementById("timer").innerText = --time + "s";
     }
 }
 
-//Set timer
+
 const timeReduce = () => {
     time = 60;
     timer = setInterval(updateTimer, 1000);
 };
 
-//End test
 const displayResult = () => {
-    //Display result div
+    
     document.querySelector(".result").style.display = "block";
     clearInterval(timer);
     document.getElementById("stop-test").style.display = "none";
@@ -97,7 +96,7 @@ const displayResult = () => {
     document.getElementById("accuracy").innerText = Math.round(((userInput.value.length - mistakes) / userInput.value.length) * 100) + "%";
 };
 
-//Start test
+
 const startTest = () => {
     mistakes = 0;
     timer = "";
